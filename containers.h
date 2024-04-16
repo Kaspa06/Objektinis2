a1b8179c0c2413bec9acf1d00aeff1946834be02
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-04-16 16:09:44 +0300

    Update containers.h

--- containers.h
+++ containers.h
@@ -1,1 +1,15 @@
+#ifndef CONTAINERS_H
+#define CONTAINERS_H
+
+#include <vector>
+
+enum class ContainerType { None, Vector, List, Deque };
+enum class Action { None, Generate, Sort };
+ContainerType getContainerChoice();
+Action getActionChoice();
+
+void performAction(ContainerType containerChoice, Action actionChoice, const std::vector<int>& sizes);
+void runApp();
+
+#endif
 
