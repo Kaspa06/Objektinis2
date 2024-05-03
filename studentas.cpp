e07cf346482715b3f9a4b84f86db4cbd1b542ed9
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-05-03 08:51:40 +0300

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -1,38 +1,91 @@
 #include "studentas.h"
 #include <algorithm>
-#include <numeric>
+#include <cstdlib> 
 
-double vidurkis(const std::vector<int>& nd) {
-    if (nd.empty()) return 0.0;
-    double sum = std::accumulate(nd.begin(), nd.end(), 0.0);
-    return sum / nd.size();
+// Constructor
+Studentas::Studentas() : egzaminas(0) {}
+
+// Constructor
+Studentas::Studentas(const std::string& vardas, const std::string& pavarde)
+    : vardas(vardas), pavarde(pavarde), egzaminas(0) {}
+
+// Destructor
+Studentas::~Studentas() {
+    nd_rezultatai.clear();
 }
 
-double mediana(std::vector<int> nd) {
-    if(nd.empty()) return 0.0;
-    sort(nd.begin(), nd.end());
-    size_t dydis = nd.size();
-    if (dydis % 2 == 0) {
-        return (nd[dydis / 2 - 1] + nd[dydis / 2]) / 2.0;
-    } else {
-        return nd[dydis / 2];
+// Public member functions
+void Studentas::setVardas(std::string vardas) {
+    this-> vardas = vardas;
+}
+std::string Studentas::getVardas() const {
+    return vardas;
+}
+
+void Studentas::setPavarde(std::string pavarde) {
+    this -> pavarde = pavarde;
+}
+
+std::string Studentas::getPavarde() const {
+    return pavarde;
+}
+
+void Studentas::setNamuDarbai(const std::vector<int>& nd) {
+    nd_rezultatai = nd;
+}
+
+std::vector<int> Studentas::getNamuDarbai() const {
+    return nd_rezultatai;
+}
+
+void Studentas::setEgzaminas(int egzaminas) {
+    this->egzaminas = egzaminas;
+}
+
+void Studentas::addNamuDarbai(int pazymys)
+{
+    nd_rezultatai.push_back(pazymys);
+}
+
+int Studentas::getEgzaminas() const {
+    return egzaminas;
+}
+
+double Studentas::calcVidurkis() const {
+    double suma = 0.0;
+    for (int pazymys : nd_rezultatai) {
+        suma += pazymys;
     }
+    return nd_rezultatai.empty() ? 0.0 : suma / nd_rezultatai.size();
 }
 
-void generateRandomGrades(Studentas& studentas) {
-    studentas.nd_rezultatai.resize(rand() % 10 + 1);
-    for (int& grade : studentas.nd_rezultatai) {
-        grade = rand() % 10 + 1;
+double Studentas::calcMediana() const {
+    if (nd_rezultatai.empty()) return 0.0;
+    std::vector<int> tempNamuDarbai = nd_rezultatai;
+    std::sort(tempNamuDarbai.begin(), tempNamuDarbai.end());
+    int dydis = tempNamuDarbai.size();
+    return (dydis % 2 == 0) ? (tempNamuDarbai[dydis / 2 - 1] + tempNamuDarbai[dydis / 2]) / 2.0 : tempNamuDarbai[dydis / 2];
+}
+
+double Studentas::calcGalutinis(bool useVidurkis) const {
+    double galutinis = useVidurkis ? (0.4 * calcVidurkis() + 0.6 * egzaminas) : (0.4 * calcMediana() + 0.6 * egzaminas);
+    return galutinis;
+}
+
+void Studentas::randomND() {
+    nd_rezultatai.resize(rand() % 10 + 1);
+    for (int& pazymys : nd_rezultatai) {
+        pazymys = rand() % 10 + 1;
     }
-    studentas.egzaminas = rand() % 10 + 1;
+    egzaminas = rand() % 10 + 1;
 }
 
-void generateRandomNamesAndGrades(Studentas& studentas) {
+void Studentas::randomStudentai() {
     const char* vardai[] = {"Jonas", "Petras", "Antanas", "Juozas", "Kazimieras"};
     const char* pavardes[] = {"Jonaitis", "Petraitis", "Antanaitis", "Juozaitis", "Kazimieraitis"};
-    int vardasIndex = rand() % 5;
-    int pavardeIndex = rand() % 5;
-    studentas.vardas = vardai[vardasIndex];
-    studentas.pavarde = pavardes[pavardeIndex];
-    generateRandomGrades(studentas);
+    int vardasIndex = rand() % 6;
+    int pavardeIndex = rand() % 6;
+    vardas = vardai[vardasIndex];
+    pavarde = pavardes[pavardeIndex];
+    randomND();
 }
