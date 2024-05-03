2faa003241827c9ad5e6d012ef025cd59710b29c
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-05-03 08:50:04 +0300

    Update funkcijosDeque.h

--- funkcijosDeque.h
+++ funkcijosDeque.h
@@ -4,7 +4,7 @@
 #include "studentas.h"
 #include <deque>
 
-void readDataDeque(std::deque<Studentas>& studentai, const std::string& failoVardas);
+void readDataDeque(std::deque<Studentas>& studentai, const std::string& failoVardas);s
 void generateStudentFilesDeque(int size);
 void rusiuotiStudentusDeque(const std::string& fileName);
 void rusiuotiStudentusDeque2(const std::string& fileName);
