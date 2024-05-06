b86478f3cab9ec6511ff6327e505db924be8762b
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-05-06 16:57:15 +0300

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -1,20 +1,56 @@
 #ifndef STUDENTAS_H
 #define STUDENTAS_H
 
-#include <vector>
 #include <string>
-#include <chrono>
+#include <vector>
 
-struct Studentas {
+class Studentas
+{
+public:
+    // Constructor
+    Studentas();
+    // Constructor with parameters
+    Studentas(const std::string &vardas, const std::string &pavarde);
+    //Destructor
+    ~Studentas();
+    // Copying constructor
+    Studentas(const Studentas &copy);
+
+    // Copy assignment
+    Studentas& operator=(const Studentas& copy);
+
+    // Move assignment operator
+    Studentas& operator=(Studentas&& copy) noexcept;
+
+    void setVardas(std::string vardas);
+    std::string getVardas() const;
+
+    void setPavarde(std::string pavarde);
+    std::string getPavarde() const;
+
+    void setNamuDarbai(const std::vector<int> &nd);
+    std::vector<int> getNamuDarbai() const;
+
+    void addNamuDarbai(int pazymys);
+
+    void setEgzaminas(int egzaminas);
+    int getEgzaminas() const;
+
+    double calcVidurkis() const;
+    double calcMediana() const;
+    double calcGalutinis(bool useVidurkis) const;
+    void randomND();
+    void randomStudentai();
+
+    friend std::ostream &operator<<(std::ostream &output, const Studentas &student);
+    friend std::istream &operator>>(std::istream &input, Studentas &student);
+
+    
+private:
     std::string vardas;
     std::string pavarde;
     std::vector<int> nd_rezultatai;
     int egzaminas;
 };
-
-double vidurkis(const std::vector<int>& nd);
-double mediana(std::vector<int> nd);
-void generateRandomGrades(Studentas& studentas);
-void generateRandomNamesAndGrades(Studentas& studentas);
 
 #endif // STUDENTAS_H
