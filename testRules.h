7f5bae63064e97c6f9baf5f60e7693298b0951a3
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-05-13 12:24:57 +0300

    Update testRules.h

--- testRules.h
+++ testRules.h
@@ -1,1 +1,12 @@
+#ifndef TESTRULES_H
+#define TESTRULES_H
+
+#include "studentas.h"
+#include <iostream>
+#include <fstream>
+
+void testRuleOfFive();
+void testSerializationDeserialization();
+
+#endif // TESTRULES_H
 
